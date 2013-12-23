fee11a52f01047fcceded703380e6e0bad770664
rastek <mahiruanina@gmail.com>
2013-12-23 01:52:33 +0100

    2 daoImpl added

---
+++ jar/src/main/java/pl/bookingsystem/db/dao/impl/StatusDAOImpl.java
@@ -0,0 +1,11 @@
+package pl.bookingsystem.db.dao.impl;
+
+import pl.bookingsystem.db.dao.StatusDAO;
+import pl.bookingsystem.db.entity.Status;
+
+/**
+ * Author: rastek
+ * Date: 22.12.13 @ 17:03
+ */
+public class StatusDAOImpl extends GenericDAOImpl<Status, Long> implements StatusDAO {
+}
