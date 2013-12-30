c9b23dc25ff7ac6076f3aaf1c2c987e2ed6bab63
Idlero <sebw20@gmail.com>
2013-12-30 21:52:43 +0100

    DB sql changed

--- sql/boookingsystem Create.sql
+++ sql/boookingsystem Create.sql
@@ -1,118 +1,119 @@
 CREATE TABLE addition
 (
-  id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(255)       NOT NULL,
-  description VARCHAR(255)
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255)
 );
 CREATE TABLE address
 (
-  id           BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  city         VARCHAR(255)       NOT NULL,
-  street       VARCHAR(255)       NOT NULL,
-  building_no  INT                NOT NULL,
-  apartment_no INT,
-  postcode     VARCHAR(8)         NOT NULL,
-  country      VARCHAR(50)        NOT NULL
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    city VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    building_no INT NOT NULL,
+    apartment_no INT,
+    postcode VARCHAR(8) NOT NULL,
+    country VARCHAR(50) NOT NULL
 );
 CREATE TABLE client
 (
-  id            BIGINT PRIMARY KEY                  NOT NULL AUTO_INCREMENT,
-  first_name    VARCHAR(255)                        NOT NULL,
-  last_name     VARCHAR(255)                        NOT NULL,
-  pesel         BIGINT                              NOT NULL,
-  nip           BIGINT,
-  email         VARCHAR(255)                        NOT NULL,
-  phone_number  VARCHAR(25)                         NOT NULL,
-  pass          VARCHAR(255)                        NOT NULL,
-  address_id    BIGINT                              NOT NULL,
-  register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date   TIMESTAMP
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    pesel BIGINT NOT NULL,
+    nip BIGINT,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(25) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    address_id BIGINT NOT NULL,
+    register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_date TIMESTAMP DEFAULT NULL
 );
 CREATE TABLE hotel
 (
-  id           BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name         VARCHAR(255)       NOT NULL,
-  description  VARCHAR(255),
-  phone_number VARCHAR(25)        NOT NULL,
-  email        VARCHAR(255)       NOT NULL,
-  address_id   BIGINT             NOT NULL
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    phone_number VARCHAR(25) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address_id BIGINT NOT NULL
 );
 CREATE TABLE hotel_client
 (
-  client_id BIGINT NOT NULL,
-  hotel_id  BIGINT NOT NULL,
-  PRIMARY KEY (client_id, hotel_id)
+    client_id BIGINT NOT NULL,
+    hotel_id BIGINT NOT NULL,
+    PRIMARY KEY ( client_id, hotel_id )
 );
 CREATE TABLE hotel_user
 (
-  hotel_id BIGINT NOT NULL,
-  user_id  BIGINT NOT NULL,
-  PRIMARY KEY (hotel_id, user_id)
+    hotel_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY ( hotel_id, user_id )
 );
 CREATE TABLE price
 (
-  id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  room_type   VARCHAR(10),
-  person_type VARCHAR(10)        NOT NULL,
-  value       INT                NOT NULL
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    room_type VARCHAR(10),
+    person_type VARCHAR(10) NOT NULL,
+    value INT NOT NULL
 );
 CREATE TABLE reservation
 (
-  id           BIGINT PRIMARY KEY                  NOT NULL AUTO_INCREMENT,
-  name         VARCHAR(255),
-  date_from    DATE                                NOT NULL,
-  date_to      DATE                                NOT NULL,
-  person_count INT                                 NOT NULL,
-  client_id    BIGINT,
-  status_id    BIGINT DEFAULT 1                    NOT NULL,
-  entry_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date  TIMESTAMP
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    date_from DATE NOT NULL,
+    date_to DATE NOT NULL,
+    person_count INT NOT NULL,
+    date_edit DATE,
+    client_id BIGINT NOT NULL,
+    status_id BIGINT NOT NULL,
+    entry_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_date TIMESTAMP DEFAULT NULL
 );
 CREATE TABLE room
 (
-  id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  room_no     INT                NOT NULL,
-  name        VARCHAR(255),
-  description VARCHAR(255),
-  bed         VARCHAR(5),
-  capacity    INT,
-  hotel_id    BIGINT
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    room_no INT NOT NULL,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    bed VARCHAR(5),
+    capacity INT,
+    hotel_id BIGINT
 );
 CREATE TABLE room_addition
 (
-  room_id     BIGINT NOT NULL,
-  addition_id BIGINT NOT NULL
+    room_id BIGINT NOT NULL,
+    addition_id BIGINT NOT NULL
 );
 CREATE TABLE room_price
 (
-  room_id  BIGINT NOT NULL,
-  price_id BIGINT NOT NULL,
-  PRIMARY KEY (room_id, price_id)
+    room_id BIGINT NOT NULL,
+    price_id BIGINT NOT NULL,
+    PRIMARY KEY ( room_id, price_id )
 );
 CREATE TABLE room_reservation
 (
-  room_id        BIGINT NOT NULL,
-  reservation_id BIGINT NOT NULL,
-  PRIMARY KEY (room_id, reservation_id)
+    room_id BIGINT NOT NULL,
+    reservation_id BIGINT NOT NULL,
+    PRIMARY KEY ( room_id, reservation_id )
 );
 CREATE TABLE status
 (
-  id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  status_type VARCHAR(255),
-  description VARCHAR(255)
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    status_type VARCHAR(255),
+    description VARCHAR(255)
 );
 CREATE TABLE user
 (
-  id            BIGINT PRIMARY KEY                  NOT NULL AUTO_INCREMENT,
-  first_name    VARCHAR(255)                        NOT NULL,
-  last_name     VARCHAR(255)                        NOT NULL,
-  pesel         BIGINT                              NOT NULL,
-  nip           BIGINT,
-  email         VARCHAR(255)                        NOT NULL,
-  address_id    BIGINT                              NOT NULL,
-  phone_number  VARCHAR(25)                         NOT NULL,
-  pass          VARCHAR(255)                        NOT NULL,
-  user_type     CHAR(8) DEFAULT 'EMPLOYEE'          NOT NULL,
-  register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_date   TIMESTAMP
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    pesel BIGINT NOT NULL,
+    nip BIGINT,
+    email VARCHAR(255) NOT NULL,
+    address_id BIGINT NOT NULL,
+    phone_number VARCHAR(25) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    user_type ENUM('EMPLOYEE', 'OWNER', 'ADMIN') DEFAULT 'EMPLOYEE' NOT NULL,
+    register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_date TIMESTAMP DEFAULT NULL
 );
