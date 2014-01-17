5bc519edf4489b384c58df77c580b8c03202e844
rastek <mahiruanina@gmail.com>
2014-01-17 23:36:56 +0100

    selectHotel.js updated

--- war/src/main/webapp/assets/js/selectHotel.js
+++ war/src/main/webapp/assets/js/selectHotel.js
@@ -1,46 +1,26 @@
-function ajaxSelect(selectId, resultContainerId) {
-    var $select = jQuery('#' + selectId);
-    var $resultContainer = jQuery('#' + resultContainerId);
-    var action = $select.data('action');
-    var selectedItemIndex = "1";//default: select first one
-    $select.on('change', function () {
-        var selectedIndex = $(this).val();
-        if (selectedIndex) selectedItemIndex = selectedIndex;
+createTableWithDataFromDB({
+    actions: {
+        get: 'hotel-getData',
+        edit: 'hotel-edit',
+        delete: 'hotel-delete'
+    },
+    table: {
+        id: 'hotel-select-table',
+        params: {
+            aoColumns: [
+                { "sTitle": "Id" },
+                { "sTitle": "Name" },
+                { "sTitle": "City" },
+                { "sTitle": "Street" },
+                { "sTitle": "Phone" },
+                { "sTitle": "Email" }
+                /*{ "sTitle": "Owner" }*/
+            ],
+            infoColumn: 7, //TODO: np. wyswietlenie mapki
+            editColumn: 8,
+            deleteColumn: 9
 
-        jQuery.ajax({
-            url: action,
-            type: 'POST',
-            data: {dataFrom: "{'index':'" + selectedItemIndex + "'}"},
-            dataType: "json",
-            error: function (msg) {
-                console.log(msg);
-                generateAlertError($resultContainer, "Wystąpił błąd servera!");
-            },
-            success: function (msg) {
-                console.log(msg);
-                try {
-                    if (msg.data[0][0] == "success") {
-                        ajaxDefaultIncudeOnStart();
-                        generateAlertSuccess($resultContainer, "Hotel wczytany.");
-                    } else {
-                        generateAlertError($resultContainer, "Wystąpił błąd podczas wczytywania hotelu!");
-                    }
-                } catch (error) {
-                    generateAlertError($resultContainer, "JavaScript ERROR: " + error);
-                }
-            }
-        });
-
-    });
-    return selectedItemIndex;
-}
-
-$(function () {
-
-
-    var currentHotelIndex = ajaxSelect('owner-select-hotel', 'server-messages');
-    createSelectListWithDataFromDB('hotel-getOwnerList', 'owner-select-hotel', {
-        label: 1,
-        value: 0
-    });
+        }
+    }
 });
+
