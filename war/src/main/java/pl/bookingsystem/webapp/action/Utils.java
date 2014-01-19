315e448834b5ba01dec78c5c76aa6f31332fb553
rastek <mahiruanina@gmail.com>
2014-01-19 06:38:02 +0100

    Action Utils: daysBetween two dates method added

--- war/src/main/java/pl/bookingsystem/webapp/action/Utils.java
+++ war/src/main/java/pl/bookingsystem/webapp/action/Utils.java
@@ -1,9 +1,6 @@
 package pl.bookingsystem.webapp.action;
 
-import pl.bookingsystem.db.entity.Client;
-import pl.bookingsystem.db.entity.User;
-
-import java.util.Map;
+import java.util.Date;
 
 /**
  * Author: rastek
@@ -17,5 +14,9 @@ public class Utils {
 
     public static String[][] setMsg(String message, String message2) {
         return new String[][]{new String[]{message, message2}};
+    }
+
+    public static int daysBetween(Date d1, Date d2){
+        return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
     }
 }
